a48a23372a0abacd7ad97524863bc7eeedc427dd
jamesdai05 <dai_guilong@hotmail.com>
2022-08-09 15:33:22 +0800

    correct the variable name in loginUser

--- controllers/loginUser.js
+++ controllers/loginUser.js
@@ -5,7 +5,7 @@ module.exports =(req,res)=>{
   const {username,password} =req.body
   User.findOne({username:username},(error,user)=>{
     if(user){
-      bcrypt.compare(password,user.Password, (error,same)=>{
+      bcrypt.compare(password,user.password, (error,same)=>{
         if(same){
           res.redirect('/')
         } else {
